0ce8a87da2d02876902eb3f4567ed16c72dadc44
Andrey Zakharchenko <a_zakharchenko_a@mail.ru>
2022-10-02 11:59:02 +0300

    feature/di-annotation branch added

--- servlets/src/main/java/ru/netology/controller/PostController.java
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -1,6 +1,8 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
 
+@Controller
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
@@ -24,7 +27,7 @@ public class PostController {
         response.getWriter().print(gson.toJson(data));
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException {
+    public void getById(long id, HttpServletResponse response) throws IOException, NotFoundException {
         response.setContentType(APPLICATION_JSON);
         final var post = service.getById(id);
         response.getWriter().print(gson.toJson(post));
--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.Collection;
@@ -8,36 +9,36 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+@Repository
 public class PostRepository {
+    private final ConcurrentMap<Long, Post> allPosts;
+    private final AtomicLong idCounter = new AtomicLong();
 
-  private final ConcurrentMap<Long, Post> posts;
-  private final AtomicLong idCounter = new AtomicLong();
-
-  public PostRepository() {
-    this.posts = new ConcurrentHashMap<>();
-  }
-  public Collection<Post> all() {
-    return posts.values();
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
-
-  public Post save(Post savePost) {
-    if(savePost.getId() == 0){
-      long id = idCounter.incrementAndGet();
-      savePost.setId(id);
-      posts.put(id, savePost);
-    } else if (savePost.getId() != 0) {
-      Long currentId = savePost.getId();
-      posts.put(currentId, savePost);
+    public PostRepository() {
+        this.allPosts = new ConcurrentHashMap<>();
     }
-    return savePost;
-  }
 
-  public void removeById(long id) {
-    posts.remove(id);
-  }
+    public Collection<Post> all() {
+        return allPosts.values();
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(allPosts.get(id));
+    }
+
+    public Post save(Post savePost) {
+        if (savePost.getId() == 0) {
+            long id = idCounter.incrementAndGet();
+            savePost.setId(id);
+            allPosts.put(id, savePost);
+        } else if (savePost.getId() != 0) {
+            Long currentId = savePost.getId();
+            allPosts.put(currentId, savePost);
+        }
+        return savePost;
+    }
+
+    public void removeById(long id) {
+        allPosts.remove(id);
+    }
 }
